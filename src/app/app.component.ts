005901bde4e9474624df99734f8d67dc5eb3f4fb
ladislasfontaine <fontaine.ladislas@gmail.com>
2020-10-23 10:04:41 +0200

    first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'first-angular-project';
+  isAuth = false;
+
+  appareilOne = 'Machine à laver';
+  appareilTwo = 'Ordinateur';
+  appareilThree = 'Télévision';
+
+  constructor () {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    )
+  }
+
+  onAllumer () {
+    console.log('On allume tout !');
+  }
 }
